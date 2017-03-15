e023ddc8e03c9d76b886b0056a1c8c97da90b4e5
Tharun Kumar Merugu <mtharu@codeaurora.org>
2017-03-15 14:04:50 +0530

    msm: ADSPRPC: Null check for file session context
    When any system call is made, before servicing it make sure that the
    session context is not null and channel info is valid.
    
    Change-Id: Ieba53b123d6553c5381db9f0b6d57b8f343e6155
    Acked-by: Vishnu Karthik D <vikarthi@qti.qualcomm.com>
    Signed-off-by: Tharun Kumar Merugu <mtharu@codeaurora.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1581,6 +1581,12 @@ static int fastrpc_internal_invoke(struct fastrpc_file *fl, uint32_t mode,
 	int err = 0;
 	struct timespec invoket = {0};
 
+	VERIFY(err, fl->sctx);
+	if (err)
+		goto bail;
+	VERIFY(err, fl->cid >= 0 && fl->cid < NUM_CHANNELS);
+	if (err)
+		goto bail;
 	if (fl->profile)
 		getnstimeofday(&invoket);
 
