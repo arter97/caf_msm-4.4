acd1854fc60ad8c6a7f26939cacccd17f3c67d8f
Richard J K <rjk@codeaurora.org>
2019-11-03 23:24:53 +0530

    soc: msm-pcm: Add mutex lock to protect prvt data
    Add mutex lock to protect private data in _put() and
    get() calls.
    Change-Id: I92f5a6515b6d1c4ad650a7dcf22a0a231a84dd30
    Signed-off-by: Prasad Kumpatla <nkumpat@codeaurora.org>
    Signed-off-by: Soumya Managoli <smanag@codeaurora.org>
    Signed-off-by: Richard J K <rjk@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-pcm-loopback-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-loopback-v2.c
@@ -177,6 +177,7 @@ static int msm_loopback_session_mute_put(struct snd_kcontrol *kcontrol,
 		goto done;
 	}
 
+	mutex_lock(&loopback_session_lock);
 	pr_debug("%s: mute=%d\n", __func__, mute);
 	hfp_tx_mute = mute;
 	for (n = 0; n < LOOPBACK_SESSION_MAX; n++) {
@@ -189,6 +190,7 @@ static int msm_loopback_session_mute_put(struct snd_kcontrol *kcontrol,
 			pr_err("%s: Send mute command failed rc=%d\n",
 				__func__, ret);
 	}
+	mutex_unlock(&loopback_session_lock);
 done:
 	return ret;
 }
@@ -381,6 +383,8 @@ static void stop_pcm(struct msm_pcm_loopback *pcm)
 
 	if (pcm->audio_client == NULL)
 		return;
+
+	mutex_lock(&loopback_session_lock);
 	q6asm_cmd(pcm->audio_client, CMD_CLOSE);
 
 	if (pcm->playback_substream != NULL) {
@@ -395,6 +399,7 @@ static void stop_pcm(struct msm_pcm_loopback *pcm)
 	}
 	q6asm_audio_client_free(pcm->audio_client);
 	pcm->audio_client = NULL;
+	mutex_unlock(&loopback_session_lock);
 }
 
 static int msm_pcm_close(struct snd_pcm_substream *substream)
@@ -545,6 +550,7 @@ static int msm_pcm_volume_ctl_put(struct snd_kcontrol *kcontrol,
 	int volume = ucontrol->value.integer.value[0];
 
 	pr_debug("%s: volume : 0x%x\n", __func__, volume);
+	mutex_lock(&loopback_session_lock);
 	if ((!substream) || (!substream->runtime)) {
 		pr_err("%s substream or runtime not found\n", __func__);
 		rc = -ENODEV;
@@ -558,6 +564,7 @@ static int msm_pcm_volume_ctl_put(struct snd_kcontrol *kcontrol,
 	rc = pcm_loopback_set_volume(prtd, volume);
 
 exit:
+	mutex_unlock(&loopback_session_lock);
 	return rc;
 }
 
@@ -570,6 +577,7 @@ static int msm_pcm_volume_ctl_get(struct snd_kcontrol *kcontrol,
 		vol->pcm->streams[SNDRV_PCM_STREAM_PLAYBACK].substream;
 	struct msm_pcm_loopback *prtd;
 
+	mutex_lock(&loopback_session_lock);
 	pr_debug("%s\n", __func__);
 	if ((!substream) || (!substream->runtime)) {
 		pr_err("%s substream or runtime not found\n", __func__);
@@ -584,6 +592,7 @@ static int msm_pcm_volume_ctl_get(struct snd_kcontrol *kcontrol,
 	ucontrol->value.integer.value[0] = prtd->volume;
 
 exit:
+	mutex_unlock(&loopback_session_lock);
 	return rc;
 }
 
