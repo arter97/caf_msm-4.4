59d0494247d280c1c10af0c2f6b82a3beb23f30f
Soumya Managoli <quic_c_smanag@quicinc.com>
2023-11-28 16:12:58 +0530

    dsp: q6asm: Add check for ADSP payload size
    There is no check for the ADSP returned payload size
    for ASM_SESSION_CMD_GET_MTMX_STRTR_PARAMS_V2 cmd response.
    This can lead to buffer overread. Fix is to address this.
    
    Change-Id: I0bd6ee7f19823addc5dde1dfbb32b8a9b102a725
    Signed-off-by: Soumya Managoli <quic_c_smanag@quicinc.com>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -11,6 +11,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
+ * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #include <linux/fs.h>
 #include <linux/mutex.h>
@@ -1679,6 +1680,7 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 	uint8_t buf_index;
 	unsigned long flags;
 	int session_id;
+	uint32_t payload_size = 0;
 
 	if (ac == NULL) {
 		pr_err("%s: ac NULL\n", __func__);
@@ -2139,6 +2141,14 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 				__func__, data->payload_size);
 		break;
 	case ASM_SESSION_CMDRSP_GET_MTMX_STRTR_PARAMS_V2:
+		payload_size = sizeof(struct asm_mtmx_strtr_get_params_cmdrsp);
+		if (data->payload_size < payload_size) {
+			pr_err("%s: insufficient payload size = %d\n",
+				__func__, data->payload_size);
+			spin_unlock_irqrestore(
+				&(session[session_id].session_lock), flags);
+			return -EINVAL;
+		}
 		q6asm_process_mtmx_get_param_rsp(ac, (void *) payload);
 		break;
 	case ASM_SESSION_CMDRSP_GET_PATH_DELAY_V2:
