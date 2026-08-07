490a9d2636d06b21a2f07be3de14837af596adc7
Apoorva Singh <apoosing@qti.qualcomm.com>
2026-08-07 09:58:20 +0530

    kgsl: Reject mmap from processes with a different mm
    We set VM_DONTCOPY, so lets continue to enforce that on the mmap path.
    
    Change-Id: I4615169faaf84cf8cdef5ae526406f592680a23c
    Signed-off-by: Scott Bauer <sbauer@qti.qualcomm.com>
    Signed-off-by: Apoorva Singh <apoosing@qti.qualcomm.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -885,6 +885,7 @@ static void kgsl_destroy_process_private(struct kref *kref)
 	if (private->pagetable->name != KGSL_MMU_GLOBAL_PT)
 		kgsl_mmu_putpagetable(private->pagetable);
 
+	mmdrop(private->mm);
 	kfree(private);
 	return;
 }
@@ -950,6 +951,8 @@ static struct kgsl_process_private *kgsl_process_private_new(
 	kref_init(&private->refcount);
 
 	private->pid = cur_pid;
+	private->mm = current->mm;
+	atomic_inc(&current->mm->mm_count);
 	get_task_comm(private->comm, current->group_leader);
 
 	spin_lock_init(&private->mem_lock);
@@ -969,6 +972,7 @@ static struct kgsl_process_private *kgsl_process_private_new(
 		idr_destroy(&private->syncsource_idr);
 		put_pid(private->pid);
 
+		mmdrop(private->mm);
 		kfree(private);
 		private = ERR_PTR(err);
 	}
@@ -4613,6 +4617,9 @@ static int kgsl_mmap(struct file *file, struct vm_area_struct *vma)
 	struct kgsl_process_private *private = dev_priv->process_priv;
 	struct kgsl_mem_entry *entry = NULL;
 	struct kgsl_device *device = dev_priv->device;
+
+	if (vma->vm_mm != private->mm)
+		return -EACCES;
 
 	/* Handle leagacy behavior for memstore */
 
